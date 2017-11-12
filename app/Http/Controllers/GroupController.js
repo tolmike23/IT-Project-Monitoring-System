e42bceabbd237f9ba14eb1abd98b51352387c81e
tolmike23 <tolmike23@gmail.com>
2017-11-12 18:29:26 +1300

    Group Controller

--- app/Http/Controllers/GroupController.js
+++ app/Http/Controllers/GroupController.js
@@ -9,86 +9,73 @@ const Requirements = use('App/Model/Requirement')
 
 class GroupController {
 
-
 	* show(request, response){
     const user = yield request.auth.getUser()
 		const group = yield Group.query().where('email', user.email).fetch()
 		yield response.sendView('dashboard', {group:group.toJSON()})
 	}
 
-	* join (request, response) {
-        const user = yield request.auth.getUser()
-        const groupKey = request.input('groupKey')
-        const groupId = request.input('groupControl')
+	* join (request, response){
+    const user = yield request.auth.getUser()
+    const groupKey = request.input('groupKey')
+    const groupId = request.input('groupControl')
+		console.log("ID pick: "+ groupId)
+		console.log("ID pick: "+ groupKey)
+    const grpExist = yield GroupControl.query().where({groupKey: groupKey, groupId:groupId}).fetch()
+    const grpLen = JSON.stringify(grpExist)
+    const grp = JSON.parse(grpLen)
 
-				//Error looking for group_Controls table
-        const grpExist = yield GroupControl.query().where('groupKey', groupKey).fetch()
-			  // const grpExist = yield Database.select('*').from('groupcontrol').where('groupKey', groupKey)
-        const grpLen = JSON.stringify(grpExist)
-        const grp = JSON.parse(grpLen)
+    if (grp.length <= 0){
+			const groupControl = yield GroupControl.query().fetch()
+      yield response.sendView('dashboard', {user:false, grpKeyError: 'Invalid group key', groupControl:groupControl.toJSON()})
+    }
+		else{
+		const grpMember = new Group()
+	  grpMember.groupId = groupId
+	  grpMember.email = user.email
+	  grpMember.firstname = user.firstname
+		grpMember.lastname = user.lastname
+	  grpMember.middlename = user.middlename
+	  grpMember.status = "active"
+		yield grpMember.save()
 
-        if (grp.length <= 0){
-            yield response.sendView('dashboard', {user:false, grpKeyError: 'Invalid group key'})
-            response.redirect('back')
-        }
+		/* Group data */
+		const group = yield Group.query().where('groupId', request.input('groupControl')).fetch()
 
-				const grpMember = new Group()
-				//grpMember.groupname = user.username
-			  grpMember.groupId = groupId
-			  grpMember.email = user.email
-			  grpMember.firstname = user.firstname
-				grpMember.lastname = user.lastname
-			  grpMember.middlename = user.middlename
-			  grpMember.status = "active"
-				//grpMember.projectid = request.input('project')
-				yield grpMember.save()
+	  /* Endorse data*/
+	  const endorse = yield Endorse.query().where('groupId', request.input('groupId')).fetch()
 
-				/* Group data */
-				const group = yield Group.query().where('groupId', request.input('groupControl')).fetch()
-
-			  /* Endorse data*/
-				//Error looking for endorses table name
-			  const endorse = yield Endorse.query().where('groupId', request.input('groupId')).fetch()
-
-				//const project = yield Projects.query().where('id', //request.input('project')).fetch()
-				//const requirements = yield Requirements.query().where('projectId', //request.input('project')).fetch()
-
-				/* update Projects table */
-			  //const projectId = yield Projects.find(request.input('project'))
-				//projectId.groupId = request.input('email')
-				//yield projectId.save()
-
-				//Error endorse is already return toJSON.
-			  yield response.sendView('dashboard', {group:group.toJSON(), endorse:endorse.toJSON(), user:true})
+	  yield response.sendView('dashboard', {group:group.toJSON(), endorse:endorse.toJSON(), user:true})
+		}
 	}
 
-    * edit (request, response){
-		/* get data */
-		const group = yield Group.query().where('id', request.input('id')).fetch()
-		const project = yield Projects.query().where('id', request.input('project')).fetch()
-		const requirements = yield Requirements.query().where('projectId', request.input('project')).fetch()
+  * edit (request, response){
+	/* get data */
+	const group = yield Group.query().where('id', request.input('id')).fetch()
+	const project = yield Projects.query().where('id', request.input('project')).fetch()
+	const requirements = yield Requirements.query().where('projectId', request.input('project')).fetch()
 
-    console.log('member '+JSON.stringify(group))
-    yield response.sendView('editGroup', {group:group.toJSON()})
+  console.log('member '+JSON.stringify(group))
+  yield response.sendView('editGroup', {group:group.toJSON()})
 
-    }
+  }
 
-    * post (request, response){
-      const member = yield Group.findOrCreate(
-            {
-							id: request.input('id')
-						},
-            {
-							firstname: request.input('firstname'),
-              lastname: request.input('lastname'),
-              status: request.input('status'),
-              updated_at: Date.now()
-            }
-        )
-        const group = yield Group.query().where('projectId', request.input('projectId')).fetch()
-        const requirements = yield Requirements.query().where('projectId', request.input('projectId')).fetch()
-        yield response.sendView('dashboard', {group:group.toJSON(), requirements:requirements.toJSON(), user:true})
-    }
+  * post (request, response){
+    const member = yield Group.findOrCreate(
+          {
+						id: request.input('id')
+					},
+          {
+						firstname: request.input('firstname'),
+            lastname: request.input('lastname'),
+            status: request.input('status'),
+            updated_at: Date.now()
+          }
+      )
+      const group = yield Group.query().where('projectId', request.input('projectId')).fetch()
+      const requirements = yield Requirements.query().where('projectId', request.input('projectId')).fetch()
+      yield response.sendView('dashboard', {group:group.toJSON(), requirements:requirements.toJSON(), user:true})
+  }
 
 }
 
