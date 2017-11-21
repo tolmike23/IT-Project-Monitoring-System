8d2fa9d7a9216a12f5a936c9ee5ef561d206e09a
tolmike23 <tolmike23@gmail.com>
2017-11-21 22:30:25 +1300

    Coordinator Group Control Data Entry

--- app/Http/Controllers/CoordinatorController.js
+++ app/Http/Controllers/CoordinatorController.js
@@ -1,25 +1,26 @@
 'use strict'
-
 const Endorse = use('App/Model/Endorse')
 const GroupControl = use('App/Model/GroupControl')
 class CoordinatorController {
     * showCoordinator (request, response){
         const user = yield request.auth.getUser()
-
         const endorse = yield Endorse.query().where({endorseTo : user.email, endorseType: 'Endorse to Coordinator'}).fetch()
-
         const gc = yield GroupControl.query().where({coordinator: user.email}).fetch()
-
-        yield response.sendView('coordinatorDashboard', {endorse:endorse.toJSON(), gc:gc.toJSON()})
+        const gcMax = yield GroupControl.query().max('groupId as maxId')
+        const gcMaxStringfy = JSON.stringify(gcMax)
+        const gcMaxParse = JSON.parse(gcMaxStringfy)
+        const maxId = gcMaxParse[0].maxId + 1
+        yield response.sendView('coordinatorDashboard', {endorse:endorse.toJSON(), gc:gc.toJSON(), maxId})
     }
 
     * createGroup(request,response){
       try {
         const user = yield request.auth.getUser()
         const gc = new GroupControl()
+        console.log("Group Id Value "+request.input('groupId'))
         gc.groupId = request.input('groupId')
         gc.groupName = request.input('groupName')
-        gc.clSched = request.input('clSched')
+        gc.clSched = request.input('startTime')+"-"+request.input('endTime')+" "+request.input('days')
         gc.groupKey = request.input('groupKey')
         gc.coordinator = user.email
         gc.adviser = request.input('adviser')
@@ -29,7 +30,8 @@ class CoordinatorController {
         return response.redirect('back')
 
       } catch (e) {
-        yield request.with({ error: "Group Key Already Exists" }).flash()
+        console.log("Error Inserting Groups " + e.stack)
+        yield request.with({ error: "Group Key Must Be Unique " }).flash()
   			return response.redirect('back')
       }
 
